235d479c313c2d3b067c991c0e4dbac0e7039601
alirezayoom <alirezayoom@gmail.com>
2022-12-13 14:16:35 +0430

    color changes

--- components/ServiceProvideItem.jsx
+++ components/ServiceProvideItem.jsx
@@ -6,12 +6,13 @@ const ServiceProvideItem = (props) => {
       className={`shadow-xl text-red-700 shadow-neutral-900/10 bg-sky-200 ${props.color}`}
     >
       <div className="px-8 py-12 text-red-700">
-        <img
+        <div className="grid justify-center mx-auto">{props.icon}</div>
+        {/* <img
           src={props.logo}
           alt="icon"
           width="64px"
           className="grid justify-center mx-auto"
-        />
+        /> */}
         <p className="text-2xl text-sky-900 pt-6 pb-8">{props.title}</p>
         <p className="text-[#020408]">{props.content}</p>
       </div>
