222b8d768b54fe9ffbae1ff225db1a875044c803
alirezayoom <alirezayoom@gmail.com>
2023-02-21 22:03:14 +0430

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/home/navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar />
       <div>hi</div>
     </div>
   );
---
+++ src/components/home/navbar.jsx
@@ -0,0 +1,28 @@
+import classes from "./navbar.module.css";
+
+const items = [
+  { name: "Home", link: "#" },
+  { name: "About", link: "#about" },
+  { name: "Project", link: "#project" },
+  { name: "Contact", link: "#contact" },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="container">
+      <div className={classes.navbar}>
+        <div className={classes.logo}>ALIREZA KARIMI</div>
+        <div className={classes.items}>
+          <ul>
+            {items.map((item) => (
+              <li key={item.name}>
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={classes.github}>hi</div>
+      </div>
+    </nav>
+  );
+}
