b446f92b729493abb10957e4bafce485d0489ac4
alirezayoom <alirezayoom@gmail.com>
2023-03-01 10:55:39 +0430

    info added to hero

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -8,6 +8,10 @@ export default function Hero() {
       <div className={classes.avatar}>
         <img src={avatar} alt="avatar" />
       </div>
+      <div className={classes.info}>
+        <h1>Hi, I'm Alireza Karimi</h1>
+        <h2>Front-end Web Developer</h2>
+      </div>
     </header>
   );
 }
