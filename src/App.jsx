f9426d468c2b75f0134a12c1e610dcf73055f1bb
alirezayoom <alirezayoom@gmail.com>
2023-03-01 10:31:42 +0430

    hero component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Hero from "./components/home/hero";
 import Navbar from "./components/home/navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <div>hi</div>
     </div>
   );
