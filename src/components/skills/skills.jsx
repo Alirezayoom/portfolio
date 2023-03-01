e66d8dbd5ad527ce7a8167db267ee51a1c1e2e74
alirezayoom <alirezayoom@gmail.com>
2023-03-01 11:34:45 +0430

    background color added

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,3 +1,9 @@
+import classes from "./skills.module.css";
+
 export default function Skills() {
-  return <div>skills</div>;
+  return (
+    <section className={classes.skills}>
+      <div>skills</div>
+    </section>
+  );
 }
