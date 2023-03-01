1d0fa145edbf33e293affaa5b54ca9401b3bc86b
alirezayoom <alirezayoom@gmail.com>
2023-03-01 16:08:13 +0430

    technogoies added

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -2,6 +2,23 @@ import classes from "./project.module.css";
 
 import avatar from "../../assets/avatar.jpeg";
 
+import html from "../../assets/skills/html.svg";
+import css from "../../assets/skills/css.svg";
+import sass from "../../assets/skills/sass.svg";
+import bootstrap from "../../assets/skills/bootstrap.svg";
+import tailwind from "../../assets/skills/tailwindcss.svg";
+import javascript from "../../assets/skills/javascript.svg";
+import typescript from "../../assets/skills/typescript.svg";
+import react from "../../assets/skills/react.svg";
+import next from "../../assets/skills/nextjs.svg";
+import git from "../../assets/skills/git.svg";
+
+const technologies = [
+  { name: "html", icon: html },
+  { name: "css", icon: css },
+  { name: "react", icon: react },
+];
+
 export default function Project() {
   return (
     <section className={classes.project}>
@@ -13,15 +30,19 @@ export default function Project() {
           <div className={classes["card-description"]}>
             <div className={classes["project-name"]}>Project Name</div>
             <div className={classes["project-technologies"]}>
-              html, css, javascirpt
+              {technologies.map((technology) => (
+                <div key={technology.name}>
+                  <img src={technology.icon} alt={technology.name} />
+                </div>
+              ))}
             </div>
             <div className={classes["project-description"]}>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
               sequi.
             </div>
             <div className={classes["project-links"]}>
-              <div>Github</div>
-              <div>Visit</div>
+              <button>Github</button>
+              <button>Visit</button>
             </div>
           </div>
         </div>
