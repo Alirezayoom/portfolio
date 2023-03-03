a70b66a7cf6ba9334e91bff0e8d0a5f972ba45cf
alirezayoom <alirezayoom@gmail.com>
2023-03-03 15:50:17 +0430

    form added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -5,7 +5,21 @@ export default function Contact() {
     <section className={classes.contact}>
       <div className="container">
         <h2>Contact Me</h2>
-        <form>hi</form>
+        <form>
+          <div className={classes.name}>
+            <input type="text" placeholder="Name" />
+          </div>
+          <div className={classes.email}>
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className={classes.subject}>
+            <input type="text" placeholder="Subject" />
+          </div>
+          <div className={classes.message}>
+            <textarea placeholder="Message" rows={8}></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </section>
   );
