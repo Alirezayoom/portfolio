dca3be373be4d5429c5c0ce0116c59debafc906d
alirezayoom <alirezayoom@gmail.com>
2023-03-03 11:21:50 +0430

    container added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,8 +3,10 @@ import classes from "./contact.module.css";
 export default function Contact() {
   return (
     <section className={classes.contact}>
-      <h2>Contact Me</h2>
-      <form></form>
+      <div className="container">
+        <h2>Contact Me</h2>
+        <form>hi</form>
+      </div>
     </section>
   );
 }
