65a44a1abf8c7d80fb542d80b5a359eb10db166e
alirezayoom <alirezayoom@gmail.com>
2023-03-03 11:05:37 +0430

    contact component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
+import Contact from "./components/contact/contact";
 import Hero from "./components/home/hero";
 import Navbar from "./components/home/navbar";
 import Project from "./components/project/project";
 import Skills from "./components/skills/skills";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <>
       <Navbar />
       <Hero />
       <Skills />
       <Project />
-      <div>hi</div>
-    </div>
+      <Contact />
+    </>
   );
 }
 
