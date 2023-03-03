3ba33188aae483391bca50eec2221fa87aa56175
alirezayoom <alirezayoom@gmail.com>
2023-03-04 00:15:53 +0430

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Contact from "./components/contact/contact";
-import Hero from "./components/home/hero";
 import Navbar from "./components/home/navbar";
-import Project from "./components/project/project";
+import Hero from "./components/home/hero";
 import Skills from "./components/skills/skills";
+import Project from "./components/project/project";
+import Contact from "./components/contact/contact";
+import Footer from "./components/layout/footer";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         <Project />
         <Contact />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,5 @@
+import classes from "./footer.module.css";
+
+export default function Footer() {
+  return <div className={classes.footer}>hi</div>;
+}
