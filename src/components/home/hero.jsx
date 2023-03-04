1d686cab9f759182ecaafbb9eb141c00112086d9
alirezayoom <alirezayoom@gmail.com>
2023-03-04 19:48:34 +0430

    arrow added

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -1,4 +1,5 @@
 import avatar from "../../assets/avatar.jpeg";
+import arrow from "../../assets/icons/arrow.svg";
 
 import classes from "./hero.module.css";
 
@@ -16,19 +17,7 @@ export default function Hero() {
         <a href="https://www.linkedin.com/in/alirezayoom/" target="_blank">
           <button>
             Linkedin
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="12"
-              height="12"
-              fill="currentColor"
-              className={classes.arrow}
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"
-              />
-            </svg>
+            <img src={arrow} alt="arrow" className={classes.arrow} />
           </button>
         </a>
       </div>
