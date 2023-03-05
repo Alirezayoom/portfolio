1de0c7619015f365f829fc7c45a6c2d342ab37bb
alirezayoom <alirezayoom@gmail.com>
2023-03-05 14:08:40 +0430

    links added

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,16 +1,16 @@
 import classes from "./skills.module.css";
 
 const skills = [
-  { name: "html" },
-  { name: "css" },
-  { name: "sass" },
-  { name: "bootstrap" },
-  { name: "tailwind" },
-  { name: "javascript" },
-  { name: "typescript" },
-  { name: "react" },
-  { name: "next" },
-  { name: "git" },
+  { name: "html", link: "https://www.w3schools.com/html" },
+  { name: "css", link: "https://www.w3schools.com/css" },
+  { name: "sass", link: "https://sass-lang.com" },
+  { name: "bootstrap", link: "https://getbootstrap.com" },
+  { name: "tailwind", link: "https://tailwindcss.com" },
+  { name: "javascript", link: "https://javascript.info/" },
+  { name: "typescript", link: "https://www.typescriptlang.org" },
+  { name: "react", link: "https://reactjs.org" },
+  { name: "next", link: "https://nextjs.org" },
+  { name: "git", link: "https://git-scm.com" },
 ];
 
 export default function Skills() {
@@ -21,9 +21,11 @@ export default function Skills() {
         <div className={classes.skillswrapper}>
           {skills.map((skill) => (
             <div key={skill.name} className={classes.icons}>
-              <div className={classes.icon}>
-                <img src={`/skills/${skill.name}.svg`} alt={skill.name} />
-              </div>
+              <a href={skill.link} target="_blank">
+                <div className={classes.icon}>
+                  <img src={`/skills/${skill.name}.svg`} alt={skill.name} />
+                </div>
+              </a>
             </div>
           ))}
         </div>
