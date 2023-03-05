3037d44ba8ba6ef64379affbd1fe771c365f0baf
alirezayoom <alirezayoom@gmail.com>
2023-03-05 09:15:43 +0430

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  {
+    plugins: [react()],
+  },
+  ({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+      define: {
+        __APP_ENV__: env.APP_ENV,
+      },
+    };
+  }
+);
