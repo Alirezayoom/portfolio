348180a22793503ab34c14a6c42fbc94d7fc5e74
alirezayoom <alirezayoom@gmail.com>
2023-03-06 10:31:44 +0430

    useref added

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,6 +6,9 @@ import Notification from "./notification";
 
 export default function Contact() {
   const form = useRef();
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
   const [status, setStatus] = useState();
 
   useEffect(() => {
@@ -16,26 +19,33 @@ export default function Contact() {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const message = messageRef.current.value;
+
+    if (!email.includes("@")) return;
+
     setStatus("sending");
 
-    emailjs
-      .sendForm(
-        `${import.meta.env.VITE_SERVICE_ID}`,
-        `${import.meta.env.VITE_TEMPLATE_ID}`,
-        form.current,
-        `${import.meta.env.VITE_PUBLIC_KEY}`
-      )
-      .then(
-        (result) => {
-          setStatus("success");
-          console.log("sent");
-          e.target.reset();
-        },
-        (error) => {
-          setStatus("faild");
-          console.log("error");
-        }
-      );
+    if (nameRef)
+      emailjs
+        .sendForm(
+          `${import.meta.env.VITE_SERVICE_ID}`,
+          `${import.meta.env.VITE_TEMPLATE_ID}`,
+          form.current,
+          `${import.meta.env.VITE_PUBLIC_KEY}`
+        )
+        .then(
+          (result) => {
+            setStatus("success");
+            console.log("sent");
+            e.target.reset();
+          },
+          (error) => {
+            setStatus("faild");
+            console.log("error");
+          }
+        );
   };
 
   let notification;
@@ -66,16 +76,26 @@ export default function Contact() {
         <h2>Contact Me</h2>
         <form ref={form} onSubmit={sendEmail}>
           <div className={classes.name}>
-            <input type="text" placeholder="Name" name="name" />
+            <input type="text" placeholder="Name" name="name" ref={nameRef} />
           </div>
           <div className={classes.email}>
-            <input type="email" placeholder="Email" name="email" />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              ref={emailRef}
+            />
           </div>
           <div className={classes.subject}>
             <input type="text" placeholder="Subject" name="subject" />
           </div>
           <div className={classes.message}>
-            <textarea placeholder="Message" rows={8} name="message"></textarea>
+            <textarea
+              placeholder="Message"
+              rows={8}
+              name="message"
+              ref={messageRef}
+            ></textarea>
           </div>
           <button type="submit">Send</button>
         </form>
